8d2c21a71ac05bbf48c5060e1d64eb9cdda3d346
oblancarte <oscar_jb1@hotmail.com>
2020-06-14 21:41:04 -0500

    Introducción a JPQL

---
+++ src/orderapi/ecommerce/dto/OrderTotalDTO.java
@@ -0,0 +1,29 @@
+package orderapi.ecommerce.dto;
+
+public class OrderTotalDTO {
+	
+	private String name;
+	private Double total;
+	
+	public OrderTotalDTO(String name, Double total) {
+		super();
+		this.name = name;
+		this.total = total;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+}
